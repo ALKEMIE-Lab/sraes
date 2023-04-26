4c4ce6f58f305ed70f00e668daab7f2c0dd17c07
Guanjie Wang <gjwang@buaa.edu.cn>
2023-04-26 09:02:48 +0800

    update example

--- emml/train/randomforest_example.py
+++ emml/train/randomforest_example.py
@@ -13,7 +13,6 @@ __update_date__ = '2023/04/26 08:08:09'
 
 
 import numpy as np
-import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
@@ -23,13 +22,14 @@ from sklearn.tree import plot_tree
 import graphviz
 
 
-def run():
+def run(is_plt=False):
     data = load_iris()
-    X = data['data']
+    xdagta = data['data']
     y = data['target']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(xdagta, y, test_size=0.2, random_state=42)
+    print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
     rf = RandomForestClassifier(n_estimators=100, random_state=42)
-    rf.fit(X_train, y_train)
+    rf.fit(x_train, y_train)
     feature_importances = rf.feature_importances_
     print("Feature importances:", feature_importances)
     
@@ -37,17 +37,18 @@ def run():
     selected_features = np.where(feature_importances > threshold)[0]
     print("Selected features:", selected_features)
     
-    X_train_selected = X_train[:, selected_features]
-    X_test_selected = X_test[:, selected_features]
-    tree_to_visualize = rf.estimators_[0]
-
-    dot_data = tree.export_graphviz(tree_to_visualize, out_file=None,
-                                    feature_names=data.feature_names,
-                                    class_names=data.target_names,
-                                    filled=True, rounded=True,
-                                    special_characters=True)
-    graph = graphviz.Source(dot_data)
-    graph.render("iris_decision_tree")  # 保存为 iris_decision_tree.pdf
+    # x_train_selected = x_train[:, selected_features]
+    # x_test_selected = x_test[:, selected_features]
+    if is_plt:
+        tree_to_visualize = rf.estimators_[0]
+    
+        dot_data = tree.export_graphviz(tree_to_visualize, out_file=None,
+                                        feature_names=data.feature_names,
+                                        class_names=data.target_names,
+                                        filled=True, rounded=True,
+                                        special_characters=True)
+        graph = graphviz.Source(dot_data)
+        graph.render("iris_decision_tree")  # 保存为 iris_decision_tree.pdf
     return rf, data, feature_importances
 
 
@@ -61,13 +62,14 @@ def plot_feature_importances(importances, feature_names):
     plt.show()
 
 
-def tree_view(rf, data):
+def tree_view(rf, data, feature_names):
     # 选择一个决策树，这里选择第一个树
     tree_to_visualize = rf.estimators_[0]
     
     # 可视化该决策树
     plt.figure(figsize=(20, 10))
-    plot_tree(tree_to_visualize, feature_names=feature_names, class_names=data['target_names'], filled=True, rounded=True)
+    plot_tree(tree_to_visualize, feature_names=feature_names, class_names=data['target_names'], filled=True,
+              rounded=True)
     plt.show()
     
     n_trees = len(rf.estimators_)
@@ -75,9 +77,9 @@ def tree_view(rf, data):
     n_rows = (n_trees + n_cols - 1) // n_cols
     fig, axes = plt.subplots(n_rows, n_cols, figsize=(20, n_rows * 10), constrained_layout=True)
     
-    for idx, tree in enumerate(rf.estimators_):
+    for idx, tt in enumerate(rf.estimators_):
         ax = axes[idx // n_cols, idx % n_cols]
-        plot_tree(tree, feature_names=feature_names, class_names=data['target_names'], filled=True, rounded=True, ax=ax)
+        plot_tree(tt, feature_names=feature_names, class_names=data['target_names'], filled=True, rounded=True, ax=ax)
         ax.set_title(f"Tree {idx + 1}")
     
     # 如果子图数量不是偶数，隐藏最后一个空白子图
@@ -89,7 +91,7 @@ def tree_view(rf, data):
 
 
 if __name__ == '__main__':
-    rf, dd, fi = run()
-    feature_names = dd['feature_names']
-    # plot_feature_importances(fi, feature_names)
-    tree_view(rf, dd)
+    rfs, dd, fi = run(is_plt=False)
+    # fn = dd['feature_names']
+    # plot_feature_importances(fi, fn)
+    # tree_view(rfs, dd, fn)
